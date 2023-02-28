ac50c8ee1eb0616d53c995cf0644b19279838638
ann <minhanh.dang0612@gmail.com>
2023-02-28 14:32:31 +0700

    first

---
+++ src/main/java/com/example/springbootcrudexample/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.example.springbootcrudexample.controller;
+
+import com.example.springbootcrudexample.entity.Product;
+import com.example.springbootcrudexample.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// Marks the class as a controller where every method returns a domain object instead of a view
+// It is shorthand for including both @Controller and @ResponseBody
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/addpProduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
+    }
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products){
+        return service.saveProducts(products);
+    }
+    @GetMapping("/products")
+    public List<Product> findAllProducts(){
+        return service.getProducts();
+    }
+    @GetMapping("/product/{id}")
+    public Product findProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable String name){
+        return service.getProductByName(name);
+    }
+
+    @PutMapping("/updateProduct")
+    public Product updateProduct(@RequestBody Product product){
+        return service.updateProduct(product);
+    }
+
+    @PutMapping("/delete/{id}")
+    public String deleteProduct(@RequestBody int id){
+        return service.deleteProduct(id);
+    }
+}
---
+++ src/main/java/com/example/springbootcrudexample/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.springbootcrudexample.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+    @Id
+    @GeneratedValue //auto generate the variable below (id)
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
