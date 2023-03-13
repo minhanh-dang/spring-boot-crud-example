05c1aee7109452670c354e643ba326ccdd928cdc
minhanh-dang <62273706+minhanh-dang@users.noreply.github.com>
2023-03-13 16:04:36 +0700

    delete

--- spring-boot-crud-example/src/main/java/com/example/springbootcrudexample/repository/UserInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootcrudexample.repository;
-
-import com.example.springbootcrudexample.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
-    Optional<UserInfo> findByName(String username);
-}
